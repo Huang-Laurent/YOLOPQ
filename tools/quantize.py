78e2d60e7138311177da380aabd2edd55e0b63c0
61HzPentile <laurent.huang@outlook.fr>
2023-02-07 20:56:33 +0100

    import mqbench

---
+++ tools/quantize.py
@@ -0,0 +1,6 @@
+import torchvision.models as models                           # for example model
+from mqbench.prepare_by_platform import prepare_by_platform   # add quant nodes for specific Backend
+from mqbench.prepare_by_platform import BackendType           # contain various Backend, like TensorRT, NNIE, etc.
+from mqbench.utils.state import enable_calibration            # turn on calibration algorithm, determine scale, zero_point, etc.
+from mqbench.utils.state import enable_quantization           # turn on actually quantization, like FP32 -> INT8
+from mqbench.convert_deploy import convert_deploy             # remove quant nodes for deploy
