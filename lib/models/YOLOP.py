84996d57e94504cdca4edfe94b8abc5cd068c0b5
61HzPentile <laurent.huang@outlook.fr>
2023-02-07 23:58:42 +0100

    dftest

--- lib/models/YOLOP.py
+++ lib/models/YOLOP.py
@@ -555,9 +555,10 @@ class MCnet(nn.Module):
             if block.from_ != -1:
                 x = cache[block.from_] if isinstance(block.from_, int) else [x if j == -1 else cache[j] for j in block.from_]       #calculate concat detect
             x = block(x)
-            # if i in self.seg_out_idx:     #save driving area segment result
-            #     m=nn.Sigmoid()
-            #     out.append(m(x))
+            if i in self.seg_out_idx:     #save driving area segment result
+                print(self.seg_out_idx)
+                m=nn.Sigmoid()
+                out.append(m(x))
             # if i == self.detector_index:
             if i == -1:
                 det_out = x
