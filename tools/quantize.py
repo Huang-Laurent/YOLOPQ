ae5e5845a3ceb430d0a1c8f1e992ed73f2bf6dc9
61HzPentile <laurent.huang@outlook.fr>
2023-02-07 21:05:30 +0100

    import mqbench

--- tools/quantize.py
+++ tools/quantize.py
@@ -1,4 +1,3 @@
-import torchvision.models as models                           # for example model
 from mqbench.prepare_by_platform import prepare_by_platform   # add quant nodes for specific Backend
 from mqbench.prepare_by_platform import BackendType           # contain various Backend, like TensorRT, NNIE, etc.
 from mqbench.utils.state import enable_calibration            # turn on calibration algorithm, determine scale, zero_point, etc.
