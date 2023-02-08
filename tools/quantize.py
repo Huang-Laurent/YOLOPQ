80e6e8fae9b67c469ad37105d72da3eaf3c0852c
61HzPentile <laurent.huang@outlook.fr>
2023-02-08 13:44:58 +0100

    s

--- tools/quantize.py
+++ tools/quantize.py
@@ -129,7 +129,8 @@ def main():
     # prepare_custom_config_dict = {'extra_qconfig_dict': extra_qconfig_dict, 'leaf_module':leaf_module}
     # prepare_custom_config_dict = {'extra_qconfig_dict': extra_qconfig_dict}
     exclude_module = [Detect]
-    extra_quantizer_dict = {'exclude_module_name': exclude_module}#, 'exclude_function_type': [operator.mul,]}
+    
+    extra_quantizer_dict = {'exclude_module_name': exclude_module, 'exclude_function_type': [Detect.forward,]}#, 'exclude_function_type': [operator.mul,]}
     prepare_custom_config_dict = {'extra_quantizer_dict': extra_quantizer_dict, 'leaf_module':leaf_module}
     print('prepare quantize model 1')
     backend = BackendType.Tensorrt
